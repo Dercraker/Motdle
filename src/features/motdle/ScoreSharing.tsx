766567cd3ec38b1fac40b319138e5939e7dc9320
Dercraker <antoine.capitain@gmail.com>
2024-04-29 10:57:15 +0200

    Feat ScoreShareing: [WIP] Add Sharing Button

--- src/features/motdle/ScoreSharing.tsx
+++ src/features/motdle/ScoreSharing.tsx
@@ -4,20 +4,32 @@ import { GetWordBySlugAction } from "@/lib/server-actions/GetWordBySlug.action";
 import { getServerUrl } from "@/lib/server-url";
 import { CharacterStateSchema } from "@/lib/zod/Motdle/CharacterState.schema";
 import { LineType } from "@/lib/zod/Motdle/Line.schema";
+import { SiteConfig } from "@/utils/site-config";
 import { Group, Paper, Stack, Text, Title } from "@mantine/core";
 import { useQuery } from "@tanstack/react-query";
+import {
+  EmailIcon,
+  EmailShareButton,
+  RedditIcon,
+  RedditShareButton,
+  TelegramIcon,
+  TelegramShareButton,
+  TwitterIcon,
+  TwitterShareButton,
+} from "next-share";
 import { useMemo } from "react";
 
 interface ScoreSharingProps {
   isWin: boolean;
   gameBoard: LineType[];
   slug: string;
+  link: string;
 }
 
-const ScoreSharing = ({ isWin, gameBoard, slug }: ScoreSharingProps) => {
+const ScoreSharing = (props: ScoreSharingProps) => {
   const { ErrorNotify } = useNotify();
   let attemptCount = -1;
-  gameBoard.map((row, rowIdx) => {
+  props.gameBoard.map((row, rowIdx) => {
     if (row.every((cell) => cell.state === CharacterStateSchema.enum.correct))
       attemptCount = rowIdx + 1;
 
@@ -29,9 +41,9 @@ const ScoreSharing = ({ isWin, gameBoard, slug }: ScoreSharingProps) => {
     isPending: fetchWord,
     error,
   } = useQuery({
-    queryKey: [slug],
+    queryKey: [props.slug],
     queryFn: async () =>
-      await GetWordBySlugAction(slug).catch((err) =>
+      await GetWordBySlugAction(props.slug).catch((err) =>
         ErrorNotify({ title: err }),
       ),
   });
@@ -39,7 +51,7 @@ const ScoreSharing = ({ isWin, gameBoard, slug }: ScoreSharingProps) => {
     ErrorNotify({ title: "Erreur lors de la récupération du mot" });
 
   const grid = useMemo(() => {
-    const colorGrid = gameBoard.map((row) => {
+    const colorGrid = props.gameBoard.map((row) => {
       const colorRow = row.map((cell) => {
         switch (cell.state) {
           case CharacterStateSchema.enum.correct:
@@ -64,11 +76,14 @@ const ScoreSharing = ({ isWin, gameBoard, slug }: ScoreSharingProps) => {
         ))}
       </Stack>
     );
-  }, [gameBoard]);
+  }, [props.gameBoard]);
 
   const gridString = useMemo(() => {
-    let stringBuilder = `J'ai trouver en mot ${word?.data?.word.trim()} en ${attemptCount} tentative\n`;
-    gameBoard.forEach((row) => {
+    let stringBuilder = "";
+    props.isWin
+      ? (stringBuilder += `J'ai trouver en mot ${word?.data?.word.trim()} en ${attemptCount} tentative\n`)
+      : (stringBuilder += `J'ai perdu à motdle, je n'ai pas trouver le mot ${word?.data?.word.trim()}\n`);
+    props.gameBoard.forEach((row) => {
       stringBuilder += row
         .map((cell) => {
           switch (cell.state) {
@@ -89,7 +104,12 @@ const ScoreSharing = ({ isWin, gameBoard, slug }: ScoreSharingProps) => {
     stringBuilder += `${getServerUrl()}/freeplay\n`;
 
     return stringBuilder;
-  }, [word?.data?.word, gameBoard, attemptCount]);
+  }, [word?.data?.word, props.gameBoard, attemptCount, props.isWin]);
+
+  const postTitle = useMemo(() => {
+    if (props.isWin) return "J'ai gagné à Motdle !";
+    return "J'ai perdu à Motdle (╯‵□′)╯︵┻━┻";
+  }, [SiteConfig.title]);
 
   return (
     <>
@@ -97,20 +117,39 @@ const ScoreSharing = ({ isWin, gameBoard, slug }: ScoreSharingProps) => {
       <Paper radius="lg" shadow="xl" p="xl" withBorder>
         <Stack align="center">
           <Group gap="5px">
-            J'ai trouver en mot<Text fw="bold">{word?.data?.word.trim()}</Text>
+            {props.isWin
+              ? `J'ai trouver en mot ${word?.data?.word.trim()}
             en
-            <Text fw="bold">{attemptCount}</Text>
-            tentative
+            ${attemptCount}
+            tentative`
+              : `J'ai perdu à motdle, je n'ai pas trouver le mot
+                ${word?.data?.word.trim()}`}
           </Group>
           {grid}
           <Text fs="italic" c="dimmed">{`${getServerUrl()}/freeplay`}</Text>
         </Stack>
       </Paper>
-      <CopyButtonComponent
-        copyLabel="Copier le résultat"
-        copiedLabel="Résultat copier"
-        value={gridString}
-      />
+      <Group justify="space-between">
+        <CopyButtonComponent
+          copyLabel="Copier le résultat"
+          copiedLabel="Résultat copier"
+          value={gridString}
+        />
+        <Group>
+          <RedditShareButton url={gridString} title={postTitle}>
+            <RedditIcon size={32} round />
+          </RedditShareButton>
+          <TwitterShareButton url={gridString} title={postTitle}>
+            <TwitterIcon size={32} round />
+          </TwitterShareButton>
+          <TelegramShareButton url={gridString} title={postTitle}>
+            <TelegramIcon size={32} round />
+          </TelegramShareButton>
+          <EmailShareButton url={gridString} subject={postTitle}>
+            <EmailIcon size={32} round />
+          </EmailShareButton>
+        </Group>
+      </Group>
     </>
   );
 };
